2cf3e65edb685ee2a0e75dbe65afadbcd66a13bb
xenophon566 <xenophon566@gmail.com>
2022-07-05 18:02:49 +0800

    mockserver add npm-run-all & my commands v.1.6

--- _mockserver/ecp/chatroom/list.js
+++ _mockserver/ecp/chatroom/list.js
@@ -1,39 +1,5 @@
 const faker = require("faker");
 
-// module.exports = {
-//     "pageCount": 1,
-//     "_header_": {
-//         "success": true,
-//         "timeCost": 9
-//     },
-//     "pageIndex": 1,
-//     "items": [{
-//         "FId": "17e28f4e-7ec0-0ef1-1797-00155dae811f",
-//         "FLastMessageTime": "2022-01-03 09:02:12.555",
-//         "FIcon": "https://sprofile.line-scdn.net/0hbYZ98Z9bPXZ3QCrZ9VhDCQcQPhxUMWRkUy4gExZFZE4aJXMmXXF3FkIQYRRJJ3t3XSIiFRUVN0B7U0oQaRbBQnBwY0FOd3wnUy93kQ",
-//         "FIsUnRead": false,
-//         "FUserName": "吳苡澤",
-//         "FBindStatus": "bind",
-//         "FLastMessage": "傳出去"
-//     }, {
-//         "FId": "17e29087-9560-0ef1-1797-00155dae811f",
-//         "FLastMessageTime": "2022-01-01 10:25:41.157",
-//         "FIcon": "https://sprofile.line-scdn.net/0h_52b7cQ8ABofGy2fe2p-ZW9LA3A8alkIOnVHL3hJWytwe0VIMH4dLCkcDSxyeUIfYHoceS0SCSoTCHd8AU38LhgrXi0mLEFLO3RK_Q",
-//         "FIsUnRead": false,
-//         "FUserName": "楊家碩",
-//         "FBindStatus": "service",
-//         "FLastMessage": "test"
-//     }, {
-//         "FId": "17e29087-9560-0ef1-1797-00155dae811f",
-//         "FLastMessageTime": "2022-01-01 10:25:41.157",
-//         "FIcon": "https://sprofile.line-scdn.net/0h_52b7cQ8ABofGy2fe2p-ZW9LA3A8alkIOnVHL3hJWytwe0VIMH4dLCkcDSxyeUIfYHoceS0SCSoTCHd8AU38LhgrXi0mLEFLO3RK_Q",
-//         "FIsUnRead": false,
-//         "FUserName": "楊家碩2",
-//         "FBindStatus": "unbind",
-//         "FLastMessage": "test"
-//     }]
-// };
-
 module.exports = {
     pageCount: 1,
     _header_: {
@@ -42,7 +8,7 @@ module.exports = {
     },
     hasNextPage: false,
     recordCount: 100,
-    items: makeChatList(100),
+    items: makeChatList(10),
 };
 
 function makeChatList(recordNum = 10) {
